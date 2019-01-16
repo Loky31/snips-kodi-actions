a2bb0f1aab82a7c03219757b032f31f54774cf23
Ian Clement <clementian067@gmail.com>
2019-01-16 19:22:14 +0100

    fix intent id name

--- action-Search-movie.py
+++ action-Search-movie.py
@@ -59,5 +59,5 @@ def action_wrapper(hermes, intentMessage, conf):
 
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
-        h.subscribe_intent("{{Ianou:Search-movie}}", subscribe_intent_callback) \
+        h.subscribe_intent("Ianou:Search-movie", subscribe_intent_callback) \
          .start()
