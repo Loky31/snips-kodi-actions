97dd8812d6a39a19346c26bf942aa0ca41116f2b
Loky31 <bensurv31@gmail.com>
2019-08-30 16:56:35 +0200

    Add files via upload

--- action-Home.py
+++ action-Home.py
@@ -52,5 +52,5 @@ def action_wrapper(hermes, intentMessage, conf):
 
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
-        h.subscribe_intent("Ianou:Home", subscribe_intent_callback) \
+        h.subscribe_intent("Loky31:Home", subscribe_intent_callback) \
          .start()
--- action-Search-tvshow.py
+++ action-Search-tvshow.py
@@ -69,5 +69,5 @@ def action_wrapper(hermes, intentMessage, conf):
 
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
-        h.subscribe_intent("Ianou:Search-tvshow", subscribe_intent_callback) \
+        h.subscribe_intent("Loky31:Search-tvshow", subscribe_intent_callback) \
          .start()
